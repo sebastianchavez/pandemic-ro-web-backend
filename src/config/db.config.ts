65d58c6234b83de5ab7fdd8affad7d4111451243
Sebastián <seba.chavez21@gmail.com>
2022-09-02 16:01:49 -0400

    ok

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -9,7 +9,11 @@ export const db: TypeOrmModuleOptions = {
     username: DB_USER,
     password: DB_PASS,
     database: DB_SCHEMA,
-    entities: [],
+    entities: ["src/**/**/*.entity.ts"],
+    // extra: {
+    //     factories: [],
+    //     seeds: []
+    // },
     synchronize: true,
     autoLoadEntities: true,
 
