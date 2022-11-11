40145d73a27ac6d10709038a73d8b7470601e038
Sebastián <seba.chavez21@gmail.com>
2022-11-11 02:58:39 -0300

    refactoring

--- src/common/services/cpanel/cpanel.service.ts
+++ src/common/services/cpanel/cpanel.service.ts
@@ -1,4 +1,20 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CpanelService {}
+export class CpanelService {
+
+    private url1: string = '/api/accounts/status'
+    private url2: string = '/api/accounts/register'
+    private url3: string = '/api/accounts/get-accounts'
+    private url4: string = '/api/accounts/get-account'
+    private url5: string = '/api/accounts/status'
+
+    constructor(
+        private httpService: HttpService
+    ){}
+
+    updateUser(request: any){
+
+    }
+}
