446a5c6b101a31ad9b10e6698b5aee3eea36c113
Sebastián <seba.chavez21@gmail.com>
2023-03-28 00:36:55 -0300

    pruebas

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -12,7 +12,6 @@ export const db: TypeOrmModuleOptions = {
   password: DB_PASS,
   database: DB_SCHEMA,
   entities: [
-   '../**/**/*.entity.{ts,js}'
   ],
   autoLoadEntities: true,
   synchronize: true,
